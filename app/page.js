9a65d3e0b5c9f78d6b59c54c82ac8df0268004db
nigmasilmi <sn.silmi@gmail.com>
2024-08-19 16:46:45 -0400

    use client directive

--- app/page.js
+++ app/page.js
@@ -1,20 +1,33 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Time to get started!
-      </h1>
-      <p>
-        <Link href="/meals">Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals">Meals</Link>
-      </p>
-      <p>
-        <Link href="/community">Community</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
+
+        <div className={classes.hero}>
+          <h1 style={{ color: "white", textAlign: "center" }}>
+            Time to get started!
+          </h1>
+          <p>Test and share</p>
+        </div>
+        <div className={classes.cta}>
+          <p>
+            <Link href="/meals">Meals</Link>
+          </p>
+          <p>
+            <Link href="/meals">Meals</Link>
+          </p>
+          <p>
+            <Link href="/community">Community</Link>
+          </p>
+        </div>
+      </header>
+    </>
   );
 }
