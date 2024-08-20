9058cdf5fba2d446dae64deb582fb912bc5855b2
nigmasilmi <sn.silmi@gmail.com>
2024-08-20 14:29:33 -0400

    usePathName the right way

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,48 +1,29 @@
-"use client";
 import React from "react";
 import logoImg from "../../assets/logo.png";
 import Link from "next/link";
 import Image from "next/image";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
-import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
-  const pathName = usePathname();
-  console.log(pathName);
-
-  const getLinkClassName = (href) => {
-    if (pathName.startsWith(href)) {
-      return classes.active;
-    } else {
-      return undefined;
-    }
-  };
-
   return (
     <>
       <MainHeaderBackground />
       <header className={classes.header}>
-        <Link href="/">
-          <div className={classes.logo}>
-            <Image src={logoImg} alt="A plate with food" />
-            NextLevel Food
-          </div>
-        </Link>
         <nav className={classes.nav}>
+          <Link href="/">
+            <div className={classes.logo}>
+              <Image src={logoImg} alt="A plate with food" />
+              NextLevel Food
+            </div>
+          </Link>
           <ul>
             <li>
-              <Link href="/meals" className={getLinkClassName("/meals")}>
-                Browse Meals
-              </Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link
-                href="/community"
-                className={getLinkClassName("/community")}
-              >
-                Foodies Community
-              </Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
