060983c3056089290b2725d422ec958be9674313
nigmasilmi <sn.silmi@gmail.com>
2024-08-22 15:34:41 -0400

    loading with suspense

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,13 +2,19 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/getMeals";
+import { Suspense } from "react";
 
 // server components can be converted in async functions
-const MealsPage = async () => {
+// this approach is to show the loading content in place of the grid and not
+// as a replacement for the Meals page content
+const Meals = async () => {
   // notice that there is no need to use useEffect or fetch
   // check reference in dev notes for more details
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+};
 
+const MealsPage = async () => {
   return (
     <>
       <header className={classes.header}>
@@ -22,7 +28,11 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
